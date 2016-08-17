c06e3b55c73742f14f3f8b20488f18cdf70b2c2a
David Welker <david.v.welker@gmail.com>
2016-08-17 13:50:24 -0700

    Adding GET and DELETE methods.

--- src/main/java/org/ndexbio/rest/mock/MockCXServer.java
+++ src/main/java/org/ndexbio/rest/mock/MockCXServer.java
@@ -1,6 +1,7 @@
 package org.ndexbio.rest.mock;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,22 +10,35 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 @Path("/network")
 public class MockCXServer 
 {
+	@GET
+	@Path("/status")
+	@Produces("application/json")
+	public String getStatus() throws Exception 
+	{
+		return "Nice job bro!";
+	}
+	
 
 	@POST
-	@Path("/asCX")
+	@Path("/")
 	@Produces("application/json")
 	@Consumes("multipart/form-data")
-	public String createCXNetwork(MultipartFormDataInput input) throws Exception 
+	public String createNetwork(MultipartFormDataInput input) throws Exception 
 	{
 		Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
 		List<InputPart> inputParts = uploadForm.get("CXNetworkStream");
@@ -45,12 +59,32 @@ public class MockCXServer
 		out.close();
 		return uuid.toString();
 	}
-
+	
 	@GET
-	@Path("/asCX")
+	@Path("/{networkId}")
 	@Produces("application/json")
-	public String getCXNetwork() throws Exception 
+	public Response getNetwork(@PathParam("networkId") final String networkId) throws Exception 
 	{
-		return "Nice job bro!";
+		File cxFile = new File("cx-files/" + networkId);
+		if( !cxFile.exists() )
+			return Response.status(404).build();
+		FileInputStream is = new FileInputStream(cxFile);
+		return Response.ok().type(MediaType.APPLICATION_JSON_TYPE).entity(is).build();
 	}
+	
+	@DELETE
+	@Path("/{networkId}")
+	@Produces("application/json")
+	public Response deleteNetwork(@PathParam("networkId") final String networkId) throws Exception 
+	{
+		File cxFile = new File("cx-files/" + networkId);
+		if( !cxFile.exists() )
+			return Response.status(404).build();
+		boolean success = cxFile.delete();
+		if( !success )
+			return Response.status(500).build();
+		String responseMsg = networkId + " successfully deleted";
+		return Response.ok().type(MediaType.APPLICATION_JSON_TYPE).entity(responseMsg).build();
+	}
+	
 }
